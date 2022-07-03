1812d93ff2e0054d233eb951666e65c6b9c10051
Theo Browne <me@t3.gg>
2022-07-03 13:44:52 -0700

    Updated homepage with new links

--- next-edge/pages/index.tsx
+++ next-edge/pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from "next";
 
+// This is the homepage for the whole thing
 const Home: NextPage = () => {
   return (
     <div style={{ padding: "4rem" }}>
       <h1>Theo&apos;s Crappy Web Deployment Runtimes Benchmarks</h1>
       <h2>(Check console and refresh for timing history)</h2>
+
+      <img
+        src="/vercel-logotype-dark.svg"
+        alt="vercel logo"
+        style={{ height: "48px" }}
+      />
       <h2>
         <a style={{ color: "blue", textDecoration: "underline" }} href="/edge">
           NextJS Edge Version
@@ -34,9 +41,33 @@ const Home: NextPage = () => {
       <h2>
         <a
           style={{ color: "blue", textDecoration: "underline" }}
+          href="https://astro-edge.t3.gg/"
+        >
+          Astro Edge Version
+        </a>
+      </h2>
+
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img src="/deno.svg" style={{ height: "48px" }} alt="Deno logo" />
+        <h1>Deno Deploy Benchmarks</h1>
+      </div>
+      <h2>
+        <a
+          style={{ color: "blue", textDecoration: "underline" }}
           href="https://t3-fresh-test.deno.dev/"
         >
           Fresh + Deno Deploy Version
+        </a>
+      </h2>
+
+      <div style={{ marginTop: 200 }} />
+
+      <h2>
+        <a
+          style={{ color: "blue", textDecoration: "underline" }}
+          href="https://github.com/t3-oss/t3bench"
+        >
+          Github Repo
         </a>
       </h2>
     </div>
